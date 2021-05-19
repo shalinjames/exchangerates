6cf116ad1e80003dc0c8aa7ea03023807c9c8c43
Shalin James <shalin.james@gmail.com>
2021-05-19 23:34:21 +0200

    Fix currencies validation fix

--- src/components/Currency/AddNewCurrency.tsx
+++ src/components/Currency/AddNewCurrency.tsx
@@ -6,17 +6,19 @@ const AddNewCurrency = ({ handleAdd }: Record<string, any>) => {
     const [currencies, setCurrencies] = useState({ from: "", to: "" })
 
     const handleAddNew = () => {
-        handleAdd(currencies);
+        const { from, to } = currencies;
+        if (from && to && from != to) {
+            handleAdd(currencies);
+        }
     }
 
     const onSelectChange = (value: string, name: string) => {
-        console.log({ value, name })
         setCurrencies({
             ...currencies,
             [name]: value
         })
     }
-
+    // TODO: Fetch the currencies and display them here
     return <Card style={{ width: 300 }}>
         <Select style={{ width: 200 }} placeholder="From" showSearch onChange={(value) => onSelectChange(value as string, 'from')}>
             <Option value="EUR">EUR - Euro</Option>
