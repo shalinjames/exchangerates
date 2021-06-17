14af8b362fcf2b891776cddc8ea8374d7bb296de
Shalin James <shalin.james@gmail.com>
2021-06-17 22:50:16 +0200

    Added test for web service api

--- src/webservices/frankfurter.test.tsx
+++ src/webservices/frankfurter.test.tsx
@@ -1,9 +1,10 @@
 import axios, { AxiosResponse } from 'axios';
 import MockAdapter from "axios-mock-adapter";
 
-import { getLatestExchangeRate } from './frankfurter';
-import { latestExchangeRate } from './urls';
-import conversionResponseJson from '../test/conversion.response.json'
+import { getLatestExchangeRate, getCurrencies } from './frankfurter';
+import { latestExchangeRate, currenciesUrl } from './urls';
+import conversionResponseJson from '../test/conversion.response.json';
+import currenciesResponseJson from "../test/currencies.response.json";
 
 
 describe("Frankfurter", () => {
@@ -13,10 +14,31 @@ describe("Frankfurter", () => {
         mock.reset();
     });
 
-    test("Should return a response with latest exchange rates", () => {
+    test("Should return a response with latest exchange rates", async () => {
         mock.onGet(latestExchangeRate).reply(200, conversionResponseJson);
-        return getLatestExchangeRate({ amount: 1, from: "EUR", to: "INR" }).then(response => {
-            expect(response).toEqual(73.149);
-        });
+        const exchangeRate = await getLatestExchangeRate({ amount: 1, from: "EUR", to: "INR" });
+        expect(exchangeRate).toEqual(73.149);
+    });
+    test("Should reject when it receive an error from latest exchange rates endpoint", () => {
+        mock.onGet(latestExchangeRate).reply(404);
+        return getLatestExchangeRate({ amount: 1, from: "EUR", to: "INR" }).catch(e =>
+            expect(e.message).toEqual(expect.stringContaining("status code 404"))
+
+        )
+    });
+
+
+    test("Should return a response with currencies", async () => {
+        mock.onGet(currenciesUrl).reply(200, currenciesResponseJson);
+        const currencies = await getCurrencies()
+        expect(currencies).toEqual(currenciesResponseJson);
+    });
+
+    test("Should reject when it receive an error from latest currencies endpoint", () => {
+        mock.onGet(currenciesUrl).reply(404);
+        return getCurrencies().catch(e =>
+            expect(e.message).toEqual(expect.stringContaining("status code 404"))
+
+        )
     });
 });
